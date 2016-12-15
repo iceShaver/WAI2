24918351c3d5d1ee4307237aaa2c3b693d54eab0
Kamil Królikowski <kamil-krolikowski@outlook.com>
2016-12-15 12:35:33 +0100

    add comments

--- WAI2/Controllers/GalleryController.php
+++ WAI2/Controllers/GalleryController.php
@@ -1,7 +1,7 @@
 <?php
 require CONTROLLERS.'Controller.php';
 /**
- * Gallery short summary.
+ * Gallerry Controller
  *
  * Gallery description.
  *
@@ -10,37 +10,59 @@ require CONTROLLERS.'Controller.php';
  */
 class GalleryController extends Controller
 {
-
+    /**
+     * Indexes all picture and display them
+     * @return void
+     */
     public function index(){
         $view = $this->LoadView('Gallery');
         $view->index();
     }
 
+    /**
+     * Displays add form for adding a new photo
+     * @return void
+     */
     public function add(){
         $view = $this->LoadView('Gallery');
         $view->add();
     }
 
+
+    /**
+     * Insert given photos to filesystem and create entry in db
+     * @return void
+     */
     public function insert(){
         $model = $this->LoadModel('Gallery');
         $model->Create($_POST);
         $this->Redirect('?task=gallery&action=index');
     }
 
+
+    /**
+     * Deletes entry and photo with given id
+     * @return void
+     */
     public function delete(){
         $model = $this->LoadModel('Gallery');
         $model->Remove($_GET['id']);
     }
 
+
+    /**
+     * Loads sample data do db for testing purposes
+     * @return void
+     */
     public function loadsampledata(){
         $model = $this->LoadModel('Gallery');
         $data = array(
             "title" => "MongoDB",
-      "description" => "database",
-      "likes" => 100,
-      "url" => "http://www.tutorialspoint.com/mongodb/",
-      "by", "tutorials point");
+            "description" => "database",
+            "likes" => 100,
+            "url" => "http://www.tutorialspoint.com/mongodb/",
+            "by", "tutorials point");
         $model->Create($data);
-        //$this->Redirect('?');
+        $this->Redirect('?');
     }
 }
--- WAI2/index.php
+++ WAI2/index.php
@@ -5,9 +5,15 @@ require INCLUDES.'helpers.inc.php';
 $object = new stdClass();
 
 
-if($_GET['task'] == 'gallery' && isset($_GET['action'])){
+//Loads gallery module
+if($_GET['module'] == 'gallery' && isset($_GET['action'])){
     require CONTROLLERS."GalleryController.php";
     $controller = new GalleryController();
     $controller->$_GET['action']();
 }
+
+
+
+//If nothing selected display main page
+
 
