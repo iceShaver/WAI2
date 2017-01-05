2b6c3bfb3bcd75dbf7cd11a805a5081e23eef164
Kamil Królikowski <kamil-krolikowski@outlook.com>
2017-01-05 12:59:38 +0100

    error displaying fixed

--- WAI2/Models/Model.php
+++ WAI2/Models/Model.php
@@ -25,7 +25,10 @@ abstract class Model
         }
         catch (MongoException $exception)
         {
-            echo 'Unable to connect to database: '.$exception->getMessage();
+            new Message(MessageType::ERROR, "Nie udało się połączyć z bazą danych: ". $exception->getMessage());
+            $view = $this->LoadView("Default");
+            $view->DisplayError();
+            exit();
         }
         $this->db = $this->dbConnection->Gallery;
 
@@ -36,7 +39,7 @@ abstract class Model
         $modelName .= 'Model';
         try{
             if(!is_file($modelPath))
-                throw new Exception("B��d podczas �adowania modelu");
+                throw new Exception("Błąd podczas ładowania modelu");
             require_once $modelPath;
             $model = new $modelName;
         }
@@ -47,5 +50,22 @@ abstract class Model
             exit();
         }
         return $model;
+    }
+    public function LoadView($viewName){
+        $viewPath = VIEWS.$viewName.'View.php';
+        $viewName .= 'View';
+        try{
+            if(!is_file($viewPath))
+                throw new Exception("Błąd podczas ładowania widoku");
+            require_once $viewPath;
+            $view = new $viewName;
+        }
+        catch(Exception $exception){
+            new Message(MessageType::ERROR, $exception->getMessage());
+            $view = $this->LoadView("Default");
+            $view->DisplayError();
+            exit();
+        }
+        return $view;
     }
 }
